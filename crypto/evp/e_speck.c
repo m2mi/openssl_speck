64dbc37036d864318034a4725b1ee3cb5a03757a
Julien Niset <julien.niset@m2mi.com>
2016-11-28 12:41:16 +0100

    Fixed problems with e_speck.c. Code is compiling!

--- crypto/evp/e_speck.c
+++ crypto/evp/e_speck.c
@@ -6,40 +6,40 @@
 #include <assert.h>
 #include <openssl/speck.h>
 #include "evp_locl.h"
-//#include "../include/internal/evp_int.h"
+#include "internal/evp_int.h"
 
-static int speck_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc);
+static int  speck_init_key(EVP_CIPHER_CTX * ctx, const unsigned char *key, const unsigned char *iv, int enc);
 
 /* Speck subkey Structure */
 typedef struct {
-	SPECK_KEY ks;
+	SPECK_KEY   ks;
 } EVP_SPECK_KEY;
 
 /* Attribute operation for Speck */
-#define data(ctx)		EVP_C_DATA(EVP_SPECK_KEY,ctx)
+#define	data(ctx)	EVP_C_DATA(EVP_SPECK_KEY,ctx)
 
-IMPLEMENT_BLOCK_CIPHER(speck_128, ks, Speck, EVP_SPECK_KEY, 
-	NID_speck_128, 16, 16, 16, 128,
-	0, speck_init_key, NULL,
-	EVP_CIPHER_set_asn1_iv,
-	EVP_CIPHER_get_asn1_iv,
-	NULL)
+#define IMPLEMENT_SPECK_CBC(keysize,cbits,iv_len) \
+				BLOCK_CIPHER_func_cbc(speck_##keysize,Speck,EVP_SPECK_KEY,ks) \
+        		BLOCK_CIPHER_def_cbc(speck_##keysize,EVP_SPECK_KEY, \
+                             NID_speck_##keysize, keysize/8, iv_len, cbits, \
+                             (0)|EVP_CIPH_FLAG_DEFAULT_ASN1, \
+                             speck_init_key, NULL, NULL, NULL, NULL)
+
+IMPLEMENT_SPECK_CBC(128,128,16)
 
 /* The subkey for Speck is generated. */
-static int speck_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc) {
+static int speck_init_key(EVP_CIPHER_CTX * ctx, const unsigned char *key, const unsigned char *iv, int enc)
+{
 	int ret;
-	if (enc) 
-		ret=Speck_set_key(key, ctx->key_len * 8, ctx->cipher_data);
+	if (enc)
+		ret = Speck_set_key(key, ctx->key_len * 8, ctx->cipher_data);
 	else
-		ret=Speck_set_key(key, ctx->key_len * 8, ctx->cipher_data);
-	if(ret < 0)
-	{ 
-		EVPerr(EVP_F_SPECK_INIT_KEY,EVP_R_SPECK_KEY_SETUP_FAILED); 
+		ret = Speck_set_key(key, ctx->key_len * 8, ctx->cipher_data);
+	if (ret < 0) {
+		EVPerr(EVP_F_SPECK_INIT_KEY, EVP_R_SPECK_KEY_SETUP_FAILED);
 		return 0;
 	}
-	return 1; 
+	return 1;
 }
-
-#else
 
 #endif
